fcbace39ca3a93946a8d914b05dc20e6c566ccf9
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-12-31 17:41:09 +0400

    feat: setup task class and createTask function

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,26 @@
+// ./src/projects.js
 
+    
 class Project {
     constructor(name) {
         this.name = name;
         this.taskList = [];
         this.id = crypto.randomUUID();
+    }
+}
+
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+        this.id = crypto.randomUUID();
+    }
+    toggleCompleted() {
+        this.completed = !this.completed;
     }
 }
 
@@ -13,3 +30,9 @@ function createProject(projectName) {
     projectList.push(new Project(projectName));
 }
 
+function createTask(projectID, title, description, dueDate, priority) {
+    const project = projectList.find(proj => proj.id === projectID);
+    if (project) {
+        project.taskList.push(new Task(title, description, dueDate, priority));
+    }
+}

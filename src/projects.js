552a13ce5aef44d941b602598a55b5a11b3780ce
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-12-31 19:37:31 +0400

    feat: implement project and task removal functions
    Added functions to remove projects and tasks by their IDs.
    The changes include two new functions:
    `removeProject` and `removeTask`, which handle the deletion process.

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,6 @@
 // ./src/projects.js
 
-    
+// Project class to represent individual projects
 class Project {
     constructor(name) {
         this.name = name;
@@ -9,7 +9,7 @@ class Project {
     }
 }
 
-
+// Task class to represent individual tasks within a project
 class Task {
     constructor(title, description, dueDate, priority) {
         this.title = title;
@@ -24,15 +24,34 @@ class Task {
     }
 }
 
-const projectList = [];
+const projectList = []; // Array to hold all projects
+
+// Functions to manage projects and tasks
 
 function createProject(projectName) {
     projectList.push(new Project(projectName));
+}
+
+function removeProject(projectID) {
+    const projectIndex = projectList.findIndex(proj => proj.id === projectID);
+    if (projectIndex !== -1) {
+        projectList.splice(projectIndex, 1);
+    }
 }
 
 function createTask(projectID, title, description, dueDate, priority) {
     const project = projectList.find(proj => proj.id === projectID);
     if (project) {
         project.taskList.push(new Task(title, description, dueDate, priority));
+    }
+}
+
+function removeTask(projectID, taskID) {
+    const project = projectList.find(proj => proj.id === projectID);
+    if (project) {
+        const taskIndex = project.taskList.findIndex(task => task.id === taskID);
+        if (taskIndex !== -1) {
+            project.taskList.splice(taskIndex, 1);
+        }
     }
 }

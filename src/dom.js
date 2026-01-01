aec5e2767a9fcae51f0b01a13c86765fb1583183
nishadnp <69951135+nishadnp@users.noreply.github.com>
2026-01-01 19:42:32 +0400

    chore: connect project module to DOM

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+// dom.js
+
+// This file will handle all DOM manipulations for the To-Do List application
+
+import { projectList, createProject, removeProject, createTask, removeTask } from "./projects.js";
+

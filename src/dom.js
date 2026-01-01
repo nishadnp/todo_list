d522f28a6fd222c54ff604b452cf0fae9ceb63c4
nishadnp <69951135+nishadnp@users.noreply.github.com>
2026-01-01 19:45:33 +0400

    feat: add fn to add and render projects in DOM

--- src/dom.js
+++ src/dom.js
@@ -4,3 +4,25 @@
 
 import { projectList, createProject, removeProject, createTask, removeTask } from "./projects.js";
 
+// Function to render the list of projects in the DOM
+function renderProjects() {
+    // Get the project list container
+    const projectListDisplay = document.querySelector("#projects > ul");
+    
+    // Clear and render all in one swoop
+    projectListDisplay.innerHTML = projectList
+    .map(project => `<li data-id="${project.id}">${project.name}</li>`)
+    .join(""); // join to make a single string
+}
+
+function addProject(projectName) {
+    createProject(projectName);
+    renderProjects();
+}
+
+const addProjectBtn = document.getElementById("add-project-btn");
+
+addProjectBtn.addEventListener("click", () => {
+    addProject(prompt("Enter project name:"));
+    console.log(projectList);
+});
